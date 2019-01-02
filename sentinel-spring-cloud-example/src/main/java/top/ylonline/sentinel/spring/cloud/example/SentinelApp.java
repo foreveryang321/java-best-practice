de74e074b4bce15870d258f9176db487ac9ecf04
foreveryang321 <453190450@qq.com>
2019-01-02 11:33:17 +0800

    remove unused code

--- sentinel-spring-cloud-example/src/main/java/top/ylonline/sentinel/spring/cloud/example/SentinelApp.java
+++ sentinel-spring-cloud-example/src/main/java/top/ylonline/sentinel/spring/cloud/example/SentinelApp.java
@@ -1,6 +1,5 @@
 package top.ylonline.sentinel.spring.cloud.example;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@Slf4j
 public class SentinelApp {
     public static void main(String[] args) {
         SpringApplication.run(SentinelApp.class, args);
