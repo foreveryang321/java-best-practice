b856d4a2b114c37b5633966fe88364bf821aff6c
foreveryang321 <453190450@qq.com>
2019-01-28 09:42:35 +0800

    rename modules

---
+++ dubbo-spring-boot-example/consumer/src/main/java/top/ylonline/dubbo/spring/boot/DubboSpringBootConsumerExample.java
@@ -0,0 +1,37 @@
+package top.ylonline.dubbo.spring.boot;
+
+import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ImportResource;
+
+import java.util.Collections;
+
+/**
+ * Hello world!
+ *
+ * @author YL
+ */
+@SpringBootApplication
+@ImportResource(locations = {"classpath:dubbo-consumer-${spring.profiles.active}.xml"})
+@DubboComponentScan(basePackages = {"top.ylonline.dubbo.spring.boot"})
+@Slf4j
+public class DubboSpringBootConsumerExample {
+
+    /**
+     * Common
+     */
+    private static SpringApplicationBuilder configureSpringBuilder(SpringApplicationBuilder builder) {
+        // builder.application().addListeners(new EnvironmentPreparedEventListener());
+        builder.application().addPrimarySources(Collections.singletonList(DubboSpringBootConsumerExample.class));
+        return builder.sources(DubboSpringBootConsumerExample.class);
+    }
+
+    public static void main(String[] args) {
+        configureSpringBuilder(new SpringApplicationBuilder())
+                .application()
+                .run(args);
+    }
+}
+
