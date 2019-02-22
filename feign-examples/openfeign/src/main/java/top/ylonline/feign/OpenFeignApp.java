17925655151f395999e878eb46da6f72c5e5ea7d
foreveryang321 <453190450@qq.com>
2019-02-22 15:11:28 +0800

    Initial commit

---
+++ feign-examples/openfeign/src/main/java/top/ylonline/feign/OpenFeignApp.java
@@ -0,0 +1,14 @@
+package top.ylonline.feign;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YL
+ */
+@SpringBootApplication
+public class OpenFeignApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
