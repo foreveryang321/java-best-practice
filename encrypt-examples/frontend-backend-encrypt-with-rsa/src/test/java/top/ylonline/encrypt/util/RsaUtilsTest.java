ade16d9a53664103467b678c34a0295f629557ae
foreveryang321 <453190450@qq.com>
2019-05-15 15:15:09 +0800

    add test

--- encrypt-examples/frontend-backend-encrypt-with-rsa/src/test/java/top/ylonline/encrypt/util/RsaUtilsTest.java
+++ encrypt-examples/frontend-backend-encrypt-with-rsa/src/test/java/top/ylonline/encrypt/util/RsaUtilsTest.java
@@ -1,13 +1,32 @@
 package top.ylonline.encrypt.util;
 
-import org.junit.Assert;
 import org.junit.Test;
 import top.ylonline.encrypt.common.Const;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author YL
  */
 public class RsaUtilsTest {
+
+    @Test
+    public void publicKey() {
+        RSAPublicKey publicKey = RsaUtils.generatePublicKey(Const.MODULUS, Const.PUBLIC_EXPONENT);
+        assertEquals(Const.PUBLIC_EXPONENT, RsaUtils.getPublicExponent(publicKey));
+        assertEquals(Const.MODULUS, RsaUtils.getModulus(publicKey));
+
+    }
+
+    @Test
+    public void privateKey() {
+        RSAPrivateKey privateKey = RsaUtils.generatePrivateKey(Const.MODULUS, Const.PRIVATE_EXPONENT);
+        assertEquals(Const.PRIVATE_EXPONENT, RsaUtils.getPrivateExponent(privateKey));
+        assertEquals(Const.MODULUS, RsaUtils.getModulus(privateKey));
+    }
 
     @Test
     public void backend() {
@@ -28,14 +47,14 @@ public class RsaUtilsTest {
         System.out.println("encrypt: " + encrypt);
         String decrypt = RsaUtils.decryptPrivate(modulus, publicExponent, encrypt);
         System.out.println("decrypt: " + decrypt);
-        Assert.assertEquals(str, decrypt);
+        assertEquals(str, decrypt);
 
         // 公钥加密，私钥解密
         encrypt = RsaUtils.encryptPrivate(modulus, publicExponent, str);
         System.out.println("encrypt: " + encrypt);
         decrypt = RsaUtils.decryptPrivate(modulus, privateExponent, encrypt);
         System.out.println("decrypt: " + decrypt);
-        Assert.assertEquals(str, decrypt);
+        assertEquals(str, decrypt);
     }
 
     /**
@@ -47,6 +66,6 @@ public class RsaUtilsTest {
                 "60ffccfdc632d0f42a73db72e29ebfd55263701791f4eea1c6de0594cbf07fd39443360e51bd2cd6f0d10a9438eb34203eb425394fe62aa4b2d62a7fe2c92b33eb9b79840a2ae2c18d575970d225ebb92e037c5f81a7d58673bbe7ad283e0f15e82bc526cc43575442bd7ebcf478dd1b7be7535adf3aa3c4c2f1b43d262bc910";
         String decrypt = RsaUtils.decryptPrivate(Const.MODULUS, Const.PRIVATE_EXPONENT, encrypt);
         System.out.println("decrypt: " + decrypt);
-        Assert.assertEquals(Const.STR, decrypt);
+        assertEquals(Const.STR, decrypt);
     }
 }
